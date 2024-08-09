e5b90fe117bd759ba6504d14d3cd183edb203734
GregoirePelegrin <46794177+GregoirePelegrin@users.noreply.github.com>
2024-08-09 08:58:31 +0200

    Add files via upload

---
+++ estimator.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class Estimator:
+    @staticmethod
+    def get_avg_gap(observations: np.ndarray) -> float:
+        gaps: list[int] = []
+        current_postition: int = 1
+
+        for observation in observations:
+            gaps.append(observation - current_postition - 1)
+            current_postition = observation
+
+        return sum(gaps) / len(gaps)
+
+    @staticmethod
+    def real_estimate(observations: np.ndarray) -> float:
+        return observations.max() + Estimator.get_avg_gap(observations)
+
+    @staticmethod
+    def my_estimate(observations: np.ndarray) -> float:
+        return 2 * observations.mean()
